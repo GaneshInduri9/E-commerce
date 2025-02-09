1191425f0b06492937d0d4ba40c1403d4ac46749
Ganesh Induri <ganeshinduri4@gmail.com>
2025-02-09 18:31:43 +0530

    Added palceOrder component
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

--- client/src/pages/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder.jsx
@@ -1,11 +1,98 @@
-import React from 'react'
-
+import React from "react";
+import Title from "../components/Title";
+import CartTotal from "../components/CartTotal";
 const PlaceOrder = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
+      {/* ------------- Left Side ---------------- */}
+      <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+        <div className="text-xl sm:text-2xl my-3">
+          <Title text1={"DELIVERY"} text2={"INFORMATION"} />
+        </div>
+        <div className="flex gap-3">
+          <input
+            required
+            name="firstName"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="First name"
+          />
+          <input
+            required
+            name="lastName"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="Last name"
+          />
+        </div>
+        <input
+          required
+          name="email"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="email"
+          placeholder="Email address"
+        />
+        <input
+          required
+          name="street"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="text"
+          placeholder="Street"
+        />
+        <div className="flex gap-3">
+          <input
+            required
+            name="firstName"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="City"
+          />
+          <input
+            required
+            name="lastName"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="State"
+          />
+        </div>
+        <div className="flex gap-3">
+          <input
+            required
+            name="firstName"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="Zipcode"
+          />
+          <input
+            required
+            name="lastName"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="Country"
+          />
+        </div>
+        <input
+          required
+          name="email"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="email"
+          placeholder="Phone"
+        />
+      </div>
 
-export default PlaceOrder
+      {/* ------------- Right Side ------------------ */}
+      <div className="mt-8">
+        <div className="mt-8 min-w-80">
+          <CartTotal />
+        </div>
+
+        <div className="mt-12">
+          <Title text1={"PAYMENT"} text2={"METHOD"} />
+          {/* --------------- Payment Method Selection ------------- */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlaceOrder;
