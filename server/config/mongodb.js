a1e62f135b5bf594c2e46626223d487939fe595b
Ganesh Induri <ganeshinduri4@gmail.com>
2025-02-13 22:13:20 +0530

    Bug fix in db connection
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+dotenv.config();
 const connectToDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("DB Connected");
   });
-  await mongoose.connect(`${process.env.MONGODB_URL}`);
+  await mongoose.connect(process.env.MONGODB_URL);
 };
 
 export default connectToDB;
