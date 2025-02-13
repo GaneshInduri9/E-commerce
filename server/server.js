f6427cb358fd2924cd78cbc047c57d6d8553e744
Ganesh Induri <ganeshinduri4@gmail.com>
2025-02-13 21:45:36 +0530

    Connect to Db
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,2 +1,18 @@
 import express from "express";
 import cors from "cors";
+import connectToDB from "./config/mongodb.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+connectToDB();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("BACKEND RESPONSE");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on Port ${port}!`);
+});
