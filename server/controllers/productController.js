14732d4ea54b1b72430d3e0d3d648178120322b8
Ganesh Induri <ganeshinduri4@gmail.com>
2025-02-15 14:43:25 +0530

    Add delete, list, single endpoints for porduct model
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,3 +1,5 @@
+import { v2 as cloudinary } from "cloudinary";
+import productModel from "../models/productModel.js";
 // Add product
 const addProduct = async (req, res) => {
   try {
@@ -15,26 +17,77 @@ const addProduct = async (req, res) => {
     const image2 = req.files.image2 && req.files.image2[0];
     const image3 = req.files.image3 && req.files.image3[0];
     const image4 = req.files.image4 && req.files.image4[0];
-    console.log(
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined
+    );
+
+    // upload to cloudinary returns array of urls.
+    let imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let res = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return res.secure_url;
+      })
+    );
+
+    // save to db
+    const productData = {
       name,
       description,
-      price,
       category,
+      price: Number(price),
       subCategory,
-      sizes,
-      bestseller
-    );
-    console.log(image1, image2, image3, image4);
-  } catch (error) {}
+      bestseller: bestseller === "true" ? true : false,
+      sizes: JSON.parse(sizes),
+      image: imagesUrl,
+      date: Date.now(),
+    };
+    console.log(productData);
+
+    const newProduct = new productModel(productData);
+    await newProduct.save();
+
+    return res.json({ success: true, message: "Product added successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 // Delete Product
-const deleteProduct = async (req, res) => {};
+const deleteProduct = async (req, res) => {
+  try {
+    await productModel.findByIdAndDelete(req.body.id);
+    return res.json({
+      success: true,
+      message: "Product deleted successfully ",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({ success: false, message: error.message });
+  }
+};
 
 // list product
-const listProduct = async (req, res) => {};
+const listProduct = async (req, res) => {
+  try {
+    const products = await productModel.find({});
+    return res.json({ success: true, products });
+  } catch (error) {
+    console.log(error);
+    return res.json({ success: false, message: error });
+  }
+};
 
 // for single product info
-const singleProduct = async (req, res) => {};
-
+const singleProduct = async (req, res) => {
+  try {
+    const product = await productModel.findById(req.body.productId);
+    return res.json({ success: true, product });
+  } catch (error) {
+    console.log(error);
+    return res.json({ success: false, message: error });
+  }
+};
 export { addProduct, deleteProduct, listProduct, singleProduct };
