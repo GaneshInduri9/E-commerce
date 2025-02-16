126e78d3c38648545c082c66916efd872b2358e3
Ganesh Induri <ganeshinduri4@gmail.com>
2025-02-16 11:17:12 +0530

    Create adminpanel
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

---
+++ admin/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+const App = () => {
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <>
+        <NavBar />
+        <hr />
+      </>
+    </div>
+  );
+};
+
+export default App;
