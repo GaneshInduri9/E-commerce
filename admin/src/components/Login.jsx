5b2b110f287748324a2feee1484bd5417ac556cc
Ganesh Induri <ganeshinduri4@gmail.com>
2025-02-17 21:40:37 +0530

    Connect admin panel to backend
    This connects admin panel to backend
    endpoint i.e /api/user/adminlogin
    and also stores returned token in local storage
    
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,16 +1,37 @@
-import React from "react";
-
-const Login = () => {
+import React, { useState } from "react";
+import { backendUrl } from "../App";
+import axios from "axios";
+import { toast } from "react-toastify";
+const Login = ({ setToken }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      const reponse = await axios.post(backendUrl + "/api/user/admin", {
+        email,
+        password,
+      });
+      if (reponse.data.success) {
+        setToken(reponse.data.token);
+      } else {
+        toast.error(reponse.data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
   return (
     <div className="min-h-screen flex items-center justify-center w-full">
       <div className="bg-white shadow-md rounded-lg max-w-md px-8 py-6">
         <h1 className="text-2xl font-bold mb-4">Admin Panel</h1>
-        <form>
+        <form onSubmit={onSubmitHandler}>
           <div className="mb-3 min-w-72">
             <p className="text-sm font-medium text-gray-700 mb-2">
               Email Address
             </p>
             <input
+              onChange={(e) => setEmail(e.target.value)}
               className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
               type="email"
               placeholder="your@email.com"
@@ -20,6 +41,7 @@ const Login = () => {
           <div className="mb-3 min-w-72">
             <p className="text-sm font-medium text-gray-700 mb-2">Password</p>
             <input
+              onChange={(e) => setPassword(e.target.value)}
               className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
               type="password"
               placeholder="Enter your password"
