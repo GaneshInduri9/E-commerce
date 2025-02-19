6434d90d02aae01a6285e94cf6793fc60a87eed6
Ganesh Induri <ganeshinduri4@gmail.com>
2025-02-19 21:51:07 +0530

    Add cart route and cart controller
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

---
+++ server/controllers/cartController.js
@@ -0,0 +1,11 @@
+// Add products to user cart
+const addToCart = async (req, res) => {};
+
+// update user cart
+const updateCart = async (req, res) => {};
+
+// get user cart
+
+const getUserCart = async (req, res) => {};
+
+export { addToCart, updateCart, getUserCart };
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import connectToDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
 
 // App config
 const app = express();
@@ -18,6 +19,7 @@ app.use(cors());
 // Api endpoints
 app.use("/api/user/", userRouter);
 app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
 
 app.get("/", (req, res) => {
   res.send("BACKEND RESPONSE");
