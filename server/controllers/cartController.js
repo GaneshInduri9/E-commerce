f0194421c84f4fbe9e1f2ef1b550a94b5b819617
Ganesh Induri <ganeshinduri4@gmail.com>
2025-02-20 18:57:15 +0530

    Update cart data in db
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,11 +1,61 @@
+import userModel from "../models/userModel.js";
+
 // Add products to user cart
-const addToCart = async (req, res) => {};
+const addToCart = async (req, res) => {
+  try {
+    const { userId, itemId, size } = req.body;
+
+    const user = await userModel.findById(userId);
+    let cartData = await user.cartData;
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+
+    await userModel.findByIdAndUpdate(userId, { cartData });
+    res.json({ success: true, message: "Added to cart successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // update user cart
-const updateCart = async (req, res) => {};
+const updateCart = async (req, res) => {
+  try {
+    const { userId, itemId, size, quantity } = req.body;
+
+    const userData = await userModel.findById(userId);
+    let cartData = await userData.cartData;
+    cartData[itemId][size] = quantity;
+
+    await userModel.findByIdAndUpdate(userId, { cartData });
+    res.json({ success: true, message: "cart updated successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // get user cart
 
-const getUserCart = async (req, res) => {};
+const getUserCart = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const userData = await userModel.findById(userId);
+    let cartData = await userData.cartData;
+
+    return res.json({ success: true, cartData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export { addToCart, updateCart, getUserCart };
