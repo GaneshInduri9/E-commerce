763d611aff88e9ee933a0345583f80dc79e3ad07
Ganesh Induri <ganeshinduri4@gmail.com>
2025-02-22 16:39:12 +0530

    Added orders component for adminpanel
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -34,7 +34,15 @@ const placeOrderStripe = async (req, res) => {};
 const placeOrderRazorPay = async (req, res) => {};
 
 // All order data for admin panel
-const allOrders = async (req, res) => {};
+const allOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // User Order data for Frontend
 const userOrders = async (req, res) => {
