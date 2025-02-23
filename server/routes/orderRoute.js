e8914144302b0ca8384c52d5b9e2b8b91a4f7dc1
Ganesh Induri <ganeshinduri4@gmail.com>
2025-02-23 14:46:35 +0530

    Add stripe payment mode
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -6,6 +6,7 @@ import {
   allOrders,
   userOrders,
   updateStatus,
+  verifyStripe,
 } from "../controllers/orderController.js";
 import adminAuth from "../middleware/adminAuth.js";
 import authUser from "../middleware/auth.js";
@@ -23,5 +24,8 @@ orderRouter.post("/razorpay", authUser, placeOrderRazorPay);
 
 // User feature
 orderRouter.post("/userorders", authUser, userOrders);
+
+//verify Stripe
+orderRouter.post("/verifyStripe", authUser, verifyStripe);
 
 export default orderRouter;
